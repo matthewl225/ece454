ee14657c9e1f171a8c06f61551f66de1e517c1db
Connor J Smith <connorjsmith101@gmail.com>
2017-10-02 01:38:18 -0400

    Improved instruction collapsing code

--- lab2/implementation.c
+++ lab2/implementation.c
@@ -6,7 +6,7 @@
 #include "implementation_reference.h"   // DO NOT REMOVE this line
 
 // Configuration parameters, uncomment them to turn them on
-#define USE_ISWHITEARRAY
+// #define USE_ISWHITEARRAY
 
 // TODO: can be removed once everything is done in-place
 unsigned char *rendered_frame;
@@ -98,147 +98,109 @@ int insert_translation_frames(optimized_kv *collapsed_sensor_values, int new_cou
 optimized_kv* collapse_sensor_values(struct kv *sensor_values, int sensor_values_count, int *new_sensor_value_count) {
     int new_count = 0;
     char *sensor_value_key;
+
     movement_type type;
     struct kv *sensor_value;
     optimized_kv *collapsed_sensor_values = (optimized_kv*)malloc(sizeof(optimized_kv) * sensor_values_count);
     int i = 0;
+    int sensor_value_value;
+    int total_up_movement = 0;
+    int total_right_movement = 0;
+    int total_clockwise_rotation = 0;
+    bool is_X_mirrored = false;
+    bool is_Y_mirrored = false;
     while (i < sensor_values_count) {
-        // Collapse translations
-        int total_up_movement = 0;
-        int total_right_movement = 0;
-        while (i < sensor_values_count) {
-            sensor_value = &sensor_values[i];
-            sensor_value_key = sensor_value->key;
-            type = sensor_value_key[0] + sensor_value_key[1];
-            switch (type) {
-                case W: total_up_movement += sensor_value->value; break;
-                case S: total_up_movement -= sensor_value->value; break;
-                case D: total_right_movement += sensor_value->value; break;
-                case A: total_right_movement -= sensor_value->value; break;
-                default: goto not_translation_1;
-            }
-            ++i;
-            if (i % 25 == 0) {
-                new_count = insert_translation_frames(collapsed_sensor_values, new_count, total_up_movement, total_right_movement);
-                total_up_movement = 0;
-                total_right_movement = 0;
-                collapsed_sensor_values[new_count].type = FRAME_BREAK;
-                collapsed_sensor_values[new_count].value = 0;
-                ++new_count;
-            }
-        }
-    not_translation_1:
-        new_count = insert_translation_frames(collapsed_sensor_values, new_count, total_up_movement, total_right_movement);
-
-
-        // Collapse rotations
-        int total_clockwise_rotation = 0;
-        while (i < sensor_values_count) {
-            sensor_value = &sensor_values[i];
-            sensor_value_key = sensor_value->key;
-            type = sensor_value_key[0] + sensor_value_key[1];
-            switch (type) {
-                case CW: total_clockwise_rotation += sensor_value->value; break;
-                case CCW: total_clockwise_rotation -= sensor_value->value; break;
-                default: goto not_rotation_1;
-            }
-            ++i;
-            if (i % 25 == 0) {
-                new_count = insert_rotation_frames(collapsed_sensor_values, new_count, total_clockwise_rotation);
-                total_clockwise_rotation = 0;
-                collapsed_sensor_values[new_count].type = FRAME_BREAK;
-                collapsed_sensor_values[new_count].value = 0;
-                ++new_count;
-            }
-        }
-
-    not_rotation_1:
-        new_count = insert_rotation_frames(collapsed_sensor_values, new_count, total_clockwise_rotation);
-
-
-        // Collapse mirroring
-        bool is_X_mirrored = 0;
-        bool is_Y_mirrored = 0;
-        while (i < sensor_values_count) {
-            sensor_value = &sensor_values[i];
-            sensor_value_key = sensor_value->key;
-            type = sensor_value_key[0] + sensor_value_key[1];
-            switch (type) {
-                case MX: is_X_mirrored = !is_X_mirrored; break;
-                case MY: is_Y_mirrored = !is_Y_mirrored; break;
-                default: goto not_mirroring_1;
-            }
-            ++i;
-            if (i % 25 == 0) {
-                new_count = insert_mirror_frames(collapsed_sensor_values, new_count, is_X_mirrored, is_Y_mirrored);
+        sensor_value = &sensor_values[i];
+        sensor_value_value = sensor_value->value;
+        sensor_value_key = sensor_value->key;
+        type = sensor_value_key[0] + sensor_value_key[1];
+        switch (type) {
+        case MX:
+            is_X_mirrored = !is_X_mirrored;
+            break;
+        case MY:
+            is_Y_mirrored = !is_Y_mirrored;
+            break;
+        case CCW:
+            sensor_value_value *= -1;
+            /* fallthrough */
+        case CW:
+            if (is_X_mirrored) sensor_value_value *= -1;
+            if (is_Y_mirrored) sensor_value_value *= -1;
+            total_clockwise_rotation = (total_clockwise_rotation + sensor_value_value) % 4;
+            if ((total_clockwise_rotation == 2 || total_clockwise_rotation == -2) && is_X_mirrored && is_Y_mirrored) {
                 is_X_mirrored = false;
                 is_Y_mirrored = false;
-                collapsed_sensor_values[new_count].type = FRAME_BREAK;
-                collapsed_sensor_values[new_count].value = 0;
-                ++new_count;
+                total_clockwise_rotation = 0;
             }
+            break;
+        case S:
+            sensor_value_value *= -1;
+            /* fallthrough */
+        case W:
+            if (is_X_mirrored) {
+                sensor_value_value *= -1;
+            }
+            switch (total_clockwise_rotation) {
+            case 0:
+                total_up_movement += sensor_value_value;
+                break;
+            case -3: 
+            case 1:
+                total_right_movement -= sensor_value_value;
+                break;
+            case -2:
+            case 2:
+                total_up_movement -= sensor_value_value;
+                break;
+            case -1:
+            case 3:
+                total_right_movement += sensor_value_value;
+                break;
+            }
+            break;
+        case A:
+            sensor_value_value *= -1;
+            /* fallthrough */
+        case D:
+            if (is_Y_mirrored) {
+                sensor_value_value *= -1;
+            }
+            switch (total_clockwise_rotation) {
+            case 0:
+                total_right_movement += sensor_value_value;
+                break;
+            case -3: 
+            case 1:
+                total_up_movement += sensor_value_value;
+                break;
+            case -2:
+            case 2:
+                total_right_movement -= sensor_value_value;
+                break;
+            case -1:
+            case 3:
+                total_up_movement -= sensor_value_value;
+                break;
+            }
+            break;
         }
-    not_mirroring_1:
-        new_count = insert_mirror_frames(collapsed_sensor_values, new_count, is_X_mirrored, is_Y_mirrored);
-
+        ++i;
+        if (i % 25 == 0) {
+            new_count = insert_translation_frames(collapsed_sensor_values, new_count, total_up_movement, total_right_movement);
+            new_count = insert_rotation_frames(collapsed_sensor_values, new_count, total_clockwise_rotation);
+            new_count = insert_mirror_frames(collapsed_sensor_values, new_count, is_X_mirrored, is_Y_mirrored);
+            total_clockwise_rotation = 0;
+            total_up_movement = 0;
+            total_right_movement = 0;
+            is_X_mirrored = false;
+            is_Y_mirrored = false;
+            collapsed_sensor_values[new_count].type = FRAME_BREAK;
+            collapsed_sensor_values[new_count++].value = 0;
+        }
     }
-
-    int second_pass_count = 0;
-    i = 0;
-    optimized_kv *second_pass_collapsed_values = (optimized_kv *)malloc(sizeof(optimized_kv) * new_count);
-    while (i < new_count) {
-        int total_up_movement = 0;
-        int total_right_movement = 0;
-        while (i < new_count) {
-            switch (collapsed_sensor_values[i].type) {
-                case W: total_up_movement += collapsed_sensor_values[i].value; break;
-                case A: total_right_movement -= collapsed_sensor_values[i].value; break;
-                case S: total_up_movement -= collapsed_sensor_values[i].value; break;
-                case D: total_right_movement += collapsed_sensor_values[i].value; break;
-                default: goto not_translating_2;
-            }
-            ++i;
-        }
-    not_translating_2:
-        second_pass_count = insert_translation_frames(second_pass_collapsed_values, second_pass_count, total_up_movement, total_right_movement);
-
-        int total_clockwise_rotation = 0;
-        while (i < new_count) {
-            switch(collapsed_sensor_values[i].type) {
-                case CW: total_clockwise_rotation += collapsed_sensor_values[i].value; break;
-                case CCW: total_clockwise_rotation -= collapsed_sensor_values[i].value; break;
-                default: goto not_rotating_2;
-            }
-            ++i;
-        }
-    not_rotating_2:
-        second_pass_count = insert_rotation_frames(second_pass_collapsed_values, second_pass_count, total_clockwise_rotation);
-
-        bool is_X_mirrored = false;
-        bool is_Y_mirrored = false;
-        while (i < new_count) {
-            switch (collapsed_sensor_values[i].type) {
-            case MX: is_X_mirrored = !is_X_mirrored; break;
-            case MY: is_Y_mirrored = !is_Y_mirrored; break;
-            default: goto not_mirrored_2;
-            }
-            ++i;
-        }
-    not_mirrored_2:
-        second_pass_count = insert_mirror_frames(second_pass_collapsed_values, second_pass_count, is_X_mirrored, is_Y_mirrored);
-
-        while (i < new_count && collapsed_sensor_values[i].type == FRAME_BREAK) {
-            second_pass_collapsed_values[second_pass_count].type = FRAME_BREAK;
-            second_pass_collapsed_values[second_pass_count].value = 0;
-            ++second_pass_count;
-            ++i;
-        }
-    }
-    free(collapsed_sensor_values);
-
-
-    *new_sensor_value_count = second_pass_count;
-    return second_pass_collapsed_values;
+    *new_sensor_value_count = new_count;
+    return collapsed_sensor_values;
 }
 /* END SENSOR COLLAPSING CODE */
 
