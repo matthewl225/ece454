f37b031a54f794a95d77e7f7fd6bed06cd09ae6d
Connor Smith <connorjsmith101@gmail.com>
2017-12-02 14:18:54 -0500

    Improved saveboard

--- lab5/save.c
+++ lab5/save.c
@@ -22,17 +22,21 @@ save_board_values (FILE* output, const char board[], const int nrows, const int 
   int err = 0;
   int i = 0;
 
-  for (i = 0; i < nrows * ncols; i++)
-    {
-      /* ASCII '0' is not zero; do the conversion */
-      err = fprintf (output, "%c\n", (board[i] & 0x1) + '0');
-      if (err < 0)
-	{
-	  fprintf (stderr, "*** Failed to write board item %d ***\n", i);
+  // Assumption: both nrows and ncols are powers of 2, therefore nrows*ncols must be a multiple of 4
+  for (i = 0; i < nrows * ncols; i += 4) {
+    /* ASCII '0' is not zero; do the conversion */
+    err = fprintf (output,
+                   "%c\n%c\n%c\n%c\n",
+                   (board[i] & 0x1) + '0',
+                   (board[i+1] & 0x1) + '0',
+                   (board[i+2] & 0x1) + '0',
+                   (board[i+3] & 0x1) + '0');
+    if (err < 0) {
+      fprintf (stderr, "*** Failed to write board item %d ***\n", i);
 	  fclose (output);
 	  exit (EXIT_FAILURE);
 	}
-    }
+  }
 }
 
 
